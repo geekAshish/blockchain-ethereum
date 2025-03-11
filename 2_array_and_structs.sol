69fd20178ec897f04fc59b054ac180b4f130bb57
Ashish Kushwaha <ashish@wishfy.club>
2025-03-11 21:38:32 +0530

    array and struct

---
+++ 2_array_and_structs.sol
@@ -0,0 +1,31 @@
+contract SolidityStorage {
+  uint256 public favouriteNumber;
+
+  struct People {
+    string name;
+    uint256 favouriteNumber;
+  }
+
+  People public person = People({favouriteNumber: 2, name: "Ashish});
+
+
+  // array
+  // dynamic array we don't specify size of the array
+  uint256[] public favouriteNumbersList;
+  People[] public peoples;
+
+  // fixed size array
+  People[4] public peoples;
+
+  function addPerson(string memory _name, uint256 _favouriteNumber) public {
+    People memory newPerson = People({favouriteNumber: _favouriteNumber, name: _name});
+    peoples.push(newPerson);
+
+    or
+    People memory newPerson = People(_favouriteNumber, _name);
+    peoples.push(newPerson);
+
+    or
+    peoples.push(People(_favouriteNumber, _name));
+  }
+}
