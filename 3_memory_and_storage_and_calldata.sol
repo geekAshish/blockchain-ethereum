1482b34fd25bb54a5511e3491be02c01d06c057d
Ashish Kushwaha <ashish@wishfy.club>
2025-03-11 21:57:47 +0530

    memory, storage, calldata

---
+++ 3_memory_and_storage_and_calldata.sol
@@ -0,0 +1,16 @@
+EVM Overview
+
+EVM can access and store information in six places:
+
+1. Stack
+2. Memory
+3. Storage
+4. Calldata
+5. Code
+6. Logs
+
+calldata is temparly veriables that can't modifyed
+memory is temparly veriables that can be modifyed
+storage is permanent variables that can be modifyed
+
+struct, array, or mapping types need to be given these keyword
