342ce85c9992c7132982e6813034d7e682b6d146
Ashish Kushwaha <ashish@wishfy.club>
2025-03-16 15:20:00 +0530

    access contract function and variable

--- 7_EtherJS/deploy.js
+++ 7_EtherJS/deploy.js
@@ -5,8 +5,14 @@ async function main() {
   // compile them in our code
   // compile them separatly
 
-  const provider = new ethers.provider.JsonRpcProvider("RPC_SERVER_URL");
-  const wallet = new ethers.Wallet('PRIVATE_KEY', provider);
+  const provider = new ethers.provider.JsonRpcProvider(process.env.RPC_SERVER_URL);
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const encryptedJson = fs.readFileSync('./.encryptedKey.json', "utf8");
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PROVIDER
+  )
+  wallet = await wallet.connect(provider); // connet wallet to provider
 
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
   const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin', 'utf8');
@@ -18,7 +24,7 @@ async function main() {
   const contract = await contractFactory.deploy({gasLimit: 1000000000});
 
   // after deployment, we can wait for deployment confimation, for eg. after one block
-  const transactionReciept = await contract.deploymentTransaction.wait(1);
+  const transactionReciept_ = await contract.deploymentTransaction.wait(1);
 
   console.log("Here is the deployment transaction (transaction response):", contract.deploymentTransaction);
   console.log("Here is the transaction receipt (only if you wait):", transactionReciept);
@@ -41,6 +47,15 @@ async function main() {
   // const sentTxResponse = await wallet.sendTransaction(tx);
   // await sentTxResponse.wait(1)
   // console.log(sentTxResponse);
+
+  // use variable and function from contract file
+  const currentFavouriteNumber = await contract.retrieve();
+  console.log(currentFavouriteNumber, currentFavouriteNumber.toString());
+  const transactionResponse = await contract.store("7");
+  const transactionReciept = await transactionResponse.wait(1);
+  const updatedFavouriteNumber = await contract.retrieve();
+  console.log(updatedFavouriteNumber.toString());
+  
   
 }
 
